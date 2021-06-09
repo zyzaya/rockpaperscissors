a164622e950a5e30bed56a9cd1ee4e9272cf70dd
Riley Sherlock <riley.sherlock@mts.net>
2021-06-09 14:10:51 -0500

    Implement function for getting and checking player input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1, str.length).toLowerCase()
+}
+
 function computerPlay() {
     let i = Math.floor(Math.random() * 3)
     switch(i) {
@@ -13,6 +17,26 @@ function computerPlay() {
     }
 }
 
-for (var i = 0; i < 10; i++) {
-    console.log(computerPlay())
+function getPlayerInput() {
+    let input = ""
+    let validInput = false
+    do {
+        input = prompt("What will you play?")
+        if (input) {
+            input = input.toLowerCase()
+            if (input.toLowerCase() === "rock" || input === "paper" || input == "scissors") {
+                validInput = true
+            }
+        }
+        if (!validInput) {
+            console.log("Invalid input, please enter 'Rock', 'Paper', or 'Scissors'")
+        }
+
+    } while (!validInput)
+    return capitalize(input)
 }
+var i = 0;
+// for (i = 0; i < 3; i++) {
+//     console.log(computerPlay())
+//     console.log(getPlayerInput())
+// }
