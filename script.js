bcae12f62d865efb43a5b0c7b8bd268eae6043bf
Riley Sherlock <riley.sherlock@mts.net>
2021-06-09 13:59:02 -0500

    Implement computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("did this work")
+function computerPlay() {
+    let i = Math.floor(Math.random() * 3)
+    switch(i) {
+        case 0:
+            return "Rock"
+            break;
+        case 1:
+            return "Paper"
+            break;
+        case 2:
+            return "Scissors"
+            break;
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(computerPlay())
+}
